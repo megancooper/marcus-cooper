4667290e5cbe7b6d95c9265d9feab4c4af32ff6c
Megan Cooper <megancooper1000@gmail.com>
2021-05-17 20:31:28 -0500

    Make non-video article types optional

--- src/types/app.ts
+++ src/types/app.ts
@@ -3,5 +3,5 @@ import {Article} from './article';
 export interface AppContextType {
   selectedCategory: string
   setSelectedCategory: (user: string) => void
-  filteredContent: (Article)[]
+  filteredContent: Article[]
 }
--- src/types/article.ts
+++ src/types/article.ts
@@ -2,10 +2,10 @@ export interface Article {
   title: string
   link: string
   source: string
-  highlight: string
-  preview: string
-  publicationDate: string
-  publicationLogo: string|null
+  highlight?: string
+  preview?: string
+  publicationDate?: string
+  publicationLogo?: string|null
   categories: string[]
   videoId?: string
   videoPath?: string
